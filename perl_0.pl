ace140037fb5970038bd936db3f41e1535ccb945
hadrianhu888 <hadrian.hu@gmail.com>
2023-01-23 19:36:44 -0500

    first commit

---
+++ perl_0.pl
@@ -0,0 +1,8 @@
+# Description: Hello World in Perl
+# This will print "Hello, World"
+
+use strict;
+use warnings;
+
+print "Hello, World";
+
---
+++ perl_1.pl
@@ -0,0 +1,26 @@
+use strict;
+use warnings;
+
+# This will print "Hello, World"
+# print "Hello, World";
+
+print "Hello, World\n";
+print ("Hello, World\n");
+
+=begin comment 
+This is a long comment; 
+to test out the comment 
+feature and functionality
+in Perl scripting langauge.
+=cut 
+
+print           "Hello, World\n\n"; 
+
+
+print "Hello, 
+            World\n\n";
+
+print 'Hello, World\n\n';
+
+
+
